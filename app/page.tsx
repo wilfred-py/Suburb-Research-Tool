957462675e7fca76a5c9099dfc029603cd927176
Wilfred Chen <wilfred@gmail.com>
2023-05-27 18:07:05 +1000

    initial layout

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,15 @@ import Image from "next/image";
 export default function Home() {
     return (
         <main>
-            <div>
-                <h1>Test</h1>
-            </div>
+            <section id="About" className="landing-page">
+                <div className="bg-customYellow text-4xl">
+                    <h1>Suburb Research Tool</h1>
+                    <p className="text-xs">
+                        Find out important details about a suburb before you
+                        buy.
+                    </p>
+                </div>
+            </section>
         </main>
     );
 }
