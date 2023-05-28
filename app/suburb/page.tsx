8da2c4a83906cd2f81b98ebce9e39ee85ef338d7
Wilfred Chen <wilfred@gmail.com>
2023-05-28 15:29:43 +1000

    update: hrefs for /suburb Link tags

--- app/suburb/page.tsx
+++ app/suburb/page.tsx
@@ -3,34 +3,34 @@ import Link from "next/link";
 export default async function Suburbs() {
     return (
         <div>
-            <h1>Which state are you looking to find more about?</h1>
+            <h1>Which state are you interested in finding more about?</h1>
             <div>
                 <ul className="flex-row flex-auto justify-center">
                     <li className="w-40 border border-black border-solid m-10 hover:bg-hoverYellow text-center">
-                        <Link href="/suburb/ACT">ACT</Link>
+                        <Link href={`./state/$state_name`}>ACT</Link>
                     </li>
                     <li className="w-40 border border-black border-solid m-10 hover:bg-hoverYellow text-center">
-                        <Link href="/suburb/NSW">NSW</Link>
+                        <Link href={`./state/$state_name`}>NSW</Link>
                     </li>
                     <li className="w-40 border border-black border-solid m-10 hover:bg-hoverYellow text-center">
-                        <Link href="/suburb/NT">NT</Link>
+                        <Link href={`./state/$state_name`}>NT</Link>
                     </li>
                     <li className="w-40 border border-black border-solid m-10 hover:bg-hoverYellow text-center">
-                        <Link href="/suburb/QLD">QLD</Link>
+                        <Link href={`./state/$state_name`}>QLD</Link>
                     </li>
                 </ul>
                 <ul className="flex-row flex-auto justify-center">
                     <li className="w-40 border border-black border-solid m-10 hover:bg-hoverYellow text-center">
-                        <Link href="/suburb/SA">SA</Link>
+                        <Link href={`./state/$state_name`}>SA</Link>
                     </li>
                     <li className="w-40 border border-black border-solid m-10 hover:bg-hoverYellow text-center">
-                        <Link href="/suburb/TAS">TAS</Link>
+                        <Link href={`./state/$state_name`}>TAS</Link>
                     </li>
                     <li className="w-40 border border-black border-solid m-10 hover:bg-hoverYellow text-center">
-                        <Link href="/suburb/VIC">VIC</Link>
+                        <Link href={`./state/$state_name`}>VIC</Link>
                     </li>
                     <li className="w-40 border border-black border-solid m-10 hover:bg-hoverYellow text-center">
-                        <Link href="/suburb/WA">WA</Link>
+                        <Link href={`./state/$state_name`}>WA</Link>
                     </li>
                 </ul>
             </div>
