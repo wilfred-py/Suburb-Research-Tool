0cf2619e3409fc304a0d2afb9cc9c4bae68e4227
Wilfred Chen <wilfred@gmail.com>
2023-05-28 12:03:55 +1000

    add: which state are you interested in?

--- app/suburb/page.tsx
+++ app/suburb/page.tsx
@@ -1,3 +1,40 @@
+import Link from "next/link";
+import styles from "./Suburbs.module.css";
+
 export default async function Suburbs() {
-    return <div></div>;
+    return (
+        <div>
+            <h1>Which state are you looking to find more about?</h1>
+            <div className="flex flex-wrap flex-auto m-4">
+                <ul className={styles.links}>
+                    <li>
+                        <Link href="/ACT">ACT</Link>
+                    </li>
+                    <li>
+                        <Link href="/NSW">NSW</Link>
+                    </li>
+                    <li>
+                        <Link href="/NT">NT</Link>
+                    </li>
+                    <li>
+                        <Link href="/QLD">QLD</Link>
+                    </li>
+                </ul>
+                <ul className={styles.links}>
+                    <li>
+                        <Link href="/SA">SA</Link>
+                    </li>
+                    <li>
+                        <Link href="/TAS">TAS</Link>
+                    </li>
+                    <li>
+                        <Link href="/VIC">VIC</Link>
+                    </li>
+                    <li>
+                        <Link href="/WA">WA</Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
 }
