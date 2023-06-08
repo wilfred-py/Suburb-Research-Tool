2c3efcb581b636959a66d60be4c5036d07fe23d9
Wilfred Chen <wilfred@gmail.com>
2023-06-08 22:07:51 +1000

    fix: participation rate conditional formatting of bars

--- components/Income.tsx
+++ components/Income.tsx
@@ -24,111 +24,111 @@ ChartJS.register(
     Legend
 );
 
+const summaryData = summary_data_Abbotsford;
+const mainData = main_data_Abbotsford;
+const keys = Object.keys(mainData["People"]["Male"]);
+
+// Name of Suburb
+const suburbName = keys[0].toString();
+
+// Name of State
+const stateName = keys[2];
+
+const personalIncome =
+    mainData["Median weekly incomes (a)"]["Personal (b)"][
+        suburbName as keyof (typeof main_data_Abbotsford)["Median weekly incomes (a)"]["Personal (b)"]
+    ];
+
+const statePersonalIncome =
+    mainData["Median weekly incomes (a)"]["Personal (b)"][
+        stateName as keyof (typeof main_data_Abbotsford)["Median weekly incomes (a)"]["Personal (b)"]
+    ];
+
+// Chart.js
+
+const labels = [2016, 2021];
+
+const personalMedianIncomeOptions = {
+    responsive: true,
+    plugins: {
+        legend: {
+            position: "bottom" as const,
+        },
+        title: {
+            display: true,
+            text: "Personal Median Weekly Income",
+        },
+    },
+};
+
+const householdMedianIncomeOptions = {
+    responsive: true,
+    plugins: {
+        legend: {
+            position: "bottom" as const,
+        },
+        title: {
+            display: true,
+            text: "Household Median Weekly Income",
+        },
+    },
+};
+
+const personalMedianIncomeData = {
+    labels,
+    datasets: [
+        {
+            label: suburbName,
+            data: labels.map(() =>
+                faker.datatype.number({ min: 0, max: 3500 })
+            ),
+            backgroundColor: "rgba(245, 195, 71, 0.5)",
+        },
+        {
+            label: stateName,
+            data: labels.map(() =>
+                faker.datatype.number({ min: 0, max: 3500 })
+            ),
+            backgroundColor: "rgba(0, 203, 165, 0.5)",
+        },
+        {
+            label: "Australia",
+            data: labels.map(() =>
+                faker.datatype.number({ min: 0, max: 3500 })
+            ),
+            backgroundColor: "rgba(53, 162, 235, 0.5)",
+        },
+    ],
+};
+
+const householdMedianIncomeData = {
+    labels,
+    datasets: [
+        {
+            label: suburbName,
+            data: labels.map(() =>
+                faker.datatype.number({ min: 0, max: 3500 })
+            ),
+            backgroundColor: "rgba(245, 195, 71, 0.5)",
+        },
+        {
+            label: stateName,
+            data: labels.map(() =>
+                faker.datatype.number({ min: 0, max: 3500 })
+            ),
+            backgroundColor: "rgba(0, 203, 165, 0.5)",
+        },
+        {
+            label: "Australia",
+            data: labels.map(() =>
+                faker.datatype.number({ min: 0, max: 3500 })
+            ),
+            backgroundColor: "rgba(53, 162, 235, 0.5)",
+        },
+    ],
+};
+
 export default function Income() {
-    const summaryData = summary_data_Abbotsford;
-    const mainData = main_data_Abbotsford;
-    const keys = Object.keys(mainData["People"]["Male"]);
-
-    // Name of Suburb
-    const suburbName = keys[0].toString();
-
-    // Name of State
-    const stateName = keys[2];
-
-    const personalIncome =
-        mainData["Median weekly incomes (a)"]["Personal (b)"][
-            suburbName as keyof (typeof main_data_Abbotsford)["Median weekly incomes (a)"]["Personal (b)"]
-        ];
-
-    const statePersonalIncome =
-        mainData["Median weekly incomes (a)"]["Personal (b)"][
-            stateName as keyof (typeof main_data_Abbotsford)["Median weekly incomes (a)"]["Personal (b)"]
-        ];
-
-    // Chart.js
-
-    const labels = [2016, 2021];
-
-    const personalMedianIncomeOptions = {
-        responsive: true,
-        plugins: {
-            legend: {
-                position: "bottom" as const,
-            },
-            title: {
-                display: true,
-                text: "Personal Median Weekly Income",
-            },
-        },
-    };
-
-    const householdMedianIncomeOptions = {
-        responsive: true,
-        plugins: {
-            legend: {
-                position: "bottom" as const,
-            },
-            title: {
-                display: true,
-                text: "Household Median Weekly Income",
-            },
-        },
-    };
-
-    const personalMedianIncomeData = {
-        labels,
-        datasets: [
-            {
-                label: suburbName,
-                data: labels.map(() =>
-                    faker.datatype.number({ min: 0, max: 3500 })
-                ),
-                backgroundColor: "rgba(245, 195, 71, 0.5)",
-            },
-            {
-                label: stateName,
-                data: labels.map(() =>
-                    faker.datatype.number({ min: 0, max: 3500 })
-                ),
-                backgroundColor: "rgba(0, 203, 165, 0.5)",
-            },
-            {
-                label: "Australia",
-                data: labels.map(() =>
-                    faker.datatype.number({ min: 0, max: 3500 })
-                ),
-                backgroundColor: "rgba(53, 162, 235, 0.5)",
-            },
-        ],
-    };
-
-    const householdMedianIncomeData = {
-        labels,
-        datasets: [
-            {
-                label: suburbName,
-                data: labels.map(() =>
-                    faker.datatype.number({ min: 0, max: 3500 })
-                ),
-                backgroundColor: "rgba(245, 195, 71, 0.5)",
-            },
-            {
-                label: stateName,
-                data: labels.map(() =>
-                    faker.datatype.number({ min: 0, max: 3500 })
-                ),
-                backgroundColor: "rgba(0, 203, 165, 0.5)",
-            },
-            {
-                label: "Australia",
-                data: labels.map(() =>
-                    faker.datatype.number({ min: 0, max: 3500 })
-                ),
-                backgroundColor: "rgba(53, 162, 235, 0.5)",
-            },
-        ],
-    };
-
     return (
         <div className="m-4">
             <Bar
