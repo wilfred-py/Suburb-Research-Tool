b9265522781df0bc0a9d84d08353a548837f868b
Wilfred Chen <wilfred@gmail.com>
2023-06-16 10:19:32 +1000

    add: successful fetch of 30 results

--- app/suburb/page.tsx
+++ app/suburb/page.tsx
@@ -5,46 +5,58 @@ import { ReactNode } from "react";
 // PocketBase
 async function getSuburbs() {
     const pb = new PocketBase("http://127.0.0.1:8090");
-    const records = await pb.collection("summary_data").getFullList({
+    const result = await pb.collection("summary_data").getFullList({
         sort: "-created",
     });
     const res = await fetch("http://127.0.0.1:8090/api/collections/summary_data/records?page=1&perPage=30", {
         cache: "no-store",
     });
 
+    if (!res.ok) {
+        throw new Error("Failed to fetch suburb data");
+    }
+
+    // Convert result into JSON
     const data = await res.json();
+
+    // Return items as an array
     return data?.items as any[];
 }
 
 export default async function Suburbs() {
-    const suburbs = await getSuburbs();
-    return (
-        <div>
-            <h1>All Suburbs</h1>
+    try {
+        const suburbs = await getSuburbs();
+        return (
             <div>
-                {suburbs?.map((suburb) => {
-                    return <Suburb key={suburb.id} suburb={suburb} />;
-                })}
+                <h1>All Suburbs</h1>
+                <div>
+                    {suburbs?.map((suburb) => {
+                        return <Suburb key={suburb.id} suburb={suburb} />;
+                    })}
+                </div>
             </div>
-        </div>
-    );
+        );
+    } catch (error) {
+        console.error("Failed to fetch suburbs:", error);
+        return <div>Error fetching suburbs</div>;
+    }
 }
 
 function Suburb({ suburb }: any) {
-    const { id, summary_data } = suburb || {};
+    const { id, suburb_name, summary_data, created } = suburb || {};
     const suburbName = Object.keys(summary_data)[0]; // Get the suburb name dynamically
     const suburbDetails = summary_data[suburbName]; // Access the nested object
 
     return (
         <Link href={`/suburb/${id}`}>
             <div>
-                <h1>{suburbName}</h1>
-                {/* Render all key-value pairs dynamically */}
+                <h1>{suburb_name}</h1>
+                {/* <h1>{suburbName}</h1>
                 {Object.entries(suburbDetails).map(([key, value]) => (
                     <p key={key}>
                         {key}: {value as ReactNode}
                     </p>
-                ))}
+                ))} */}
             </div>
         </Link>
     );
