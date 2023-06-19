1d3b6e7e9617eafe79cb95fd465ce04c679ad445
Wilfred Chen <wilfred@gmail.com>
2023-06-19 13:10:38 +1000

    add: manage show results state

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -5,11 +5,11 @@ import { useRouter } from "next/navigation";
 import { encode } from "punycode";
 import { useEffect, useRef, useState } from "react";
 import { suburbNames } from "../data/suburbNames";
+import Link from "next/link";
 
 export default function SearchBar() {
     // state to manage search query
     const [searchQuery, setSearchQuery] = useState("");
-    console.log(searchQuery);
 
     const router = useRouter();
 
@@ -24,14 +24,20 @@ export default function SearchBar() {
         router.push(`/suburb?q=${encodedSearchQuery}`);
     };
 
-    // state to manage drop down list of matching results
-    // initialise with all suburb names
+    // state to manage dropdown show/hidden
+    const [showResults, setShowResults] = useState(false);
+
+    // if user clicks box, show most recent searches
+    //
+
+    // state to manage drop down list of matching results; initialise with all suburb names
     const [searchResults, setSearchResults] = useState<string[]>(suburbNames);
 
     // Filter search results based on searchQuery state
     useEffect(() => {
         const filteredResults = suburbNames.filter((suburb) => suburb.toLowerCase().includes(searchQuery.toLowerCase()));
         setSearchResults(filteredResults);
+        setShowResults(true);
     }, [searchQuery]);
 
     return (
@@ -45,46 +51,29 @@ export default function SearchBar() {
                         <input
                             value={searchQuery}
                             onChange={(event) => setSearchQuery(event.target.value)}
+                            onClick={() => setShowResults(false)}
                             type="search"
                             placeholder="Suburb or Postcode..."
                             className="w-[600px] m-10 px-5 py-3 text-lg rounded-full border-2 border-gray-500 focus:border-gray-700 outline-none transition"
                         />
+
                         {/* Search results container */}
-                        <div className="flex flex-col first-line:absolute mt-1 w-full p-2 bg-white shadow-lg rounded-bl rounded-br max-h-36 overflow-y-auto">
-                            {searchResults.map((suburb) => {
-                                return <div className="hover:bg-hoverYellow">{suburb}</div>;
-                            })}
-                        </div>
+                        {showResults ? (
+                            <div className="flex flex-col first-line:absolute mt-1 w-full p-2 bg-white shadow-lg rounded-bl rounded-br max-h-36 overflow-y-auto">
+                                {searchResults.map((suburb) => {
+                                    return (
+                                        <Link href={`/suburb/${suburb}`}>
+                                            <div className="hover:bg-hoverYellow">{suburb}</div>
+                                        </Link>
+                                    );
+                                })}
+                            </div>
+                        ) : (
+                            ""
+                        )}
                     </form>
                 </div>
             </div>
         </div>
     );
 }
-
-// interface Props<T> {
-//     suburbs: T[];
-//     renderItem(item: T): JSX.Element;
-// }
-
-// // Define function signature that takes in prop called "results" of type "Props<any>". This specifies that this component is a function component.
-// // The Props interface is a generic interface that allows speicfying the type of "results" as any data type.
-// // function ({ results }) is the function body where we destructure the "results" rpopr from the component's prop.
-// const LiveSearch: FC<Props<any>> = function ({ suburbs, renderItem }) {
-//     return (
-//         <div className="flex m-4 items-center justify-center">
-//             <div className="relative">
-//                 <input
-//                     type="text"
-//                     placeholder="Suburb or postcode..."
-//                     className="w-[600px] px-5 py-3 text-lg rounded-full border-2 border-gray-500 focus:border-gray-700 outline-none transition"
-//                 />
-
-//                 {/* Search Results Container */}
-//                 <div className="absolute mt-1 w-full p-2 bg-white shadow-lg rounded-bl rounded-br max-h-36 overflow-y-auto"></div>
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default LiveSearch;
