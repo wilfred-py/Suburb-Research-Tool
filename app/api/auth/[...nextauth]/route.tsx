5e33db71abba11a6c75b4349ec8d4817a5c0fe36
Wilfred Chen <wilfred@gmail.com>
2023-07-06 20:31:41 +1000

    added google oAuth and gmail user avatar to config

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -10,6 +10,10 @@ export const authOptions: NextAuthOptions = {
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET,
         }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        }),
     ],
 };
 
