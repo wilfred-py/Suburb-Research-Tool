776a339f8abb1af622760e3ff37406a139d00036
Wilfred Chen <wilfred@gmail.com>
2023-07-07 09:30:12 +1000

    added AuthForm; auth/callback works and redirects to /account

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import SearchBar from "@/components/SearchBar";
+import AuthForm from "./AuthForm";
 
 export default async function Home() {
     return (
@@ -7,6 +8,9 @@ export default async function Home() {
                 <h1 className="mt-10 text-2xl">Find out more about the suburb you want to buy or rent in</h1>
                 <div>
                     <SearchBar />
+                </div>
+                <div className="col-6 auth-widget">
+                    <AuthForm />
                 </div>
             </section>
         </main>
