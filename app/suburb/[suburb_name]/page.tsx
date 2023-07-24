0ec4178c50c19415f562aeed4dfe89dc663f72bb
Wilfred Chen <wilfred@gmail.com>
2023-07-24 18:22:28 +1000

    summary data headings

--- app/suburb/[suburb_name]/page.tsx
+++ app/suburb/[suburb_name]/page.tsx
@@ -5,7 +5,7 @@ import GetSummaryData from "@/components/SummaryData_Supabase";
 
 export default async function Suburb() {
     return (
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col xl:px-52 lg:px-52 md:px-32 sm:px-5">
             <div>
                 <GetSummaryData />
             </div>
