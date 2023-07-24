d09c18faab7d6e41932760b71fd497b512360830
Wilfred Chen <wilfred@gmail.com>
2023-07-24 11:47:17 +1000

    Supabase RPC .eq() search working

--- supabase/migrations/20230715082241_starting-ddl.sql
+++ supabase/migrations/20230715082241_starting-ddl.sql
@@ -33,38 +33,117 @@ create table user_profiles (
 );
 
 -- Return row where suburb_name matches searchQuery
-create function get_suburb_data(searchQuery text)
+CREATE FUNCTION get_suburb_data(_searchquery text)
+RETURNS table (
+    id uuid,
+    suburb_name text,
+    state_name text,
+    post_code numeric,
+    people INT,
+    male REAL,
+    female REAL,
+    median_age INT,
+    families INT,
+    average_number_of_children_per_family text,
+    for_families_with_children REAL,
+    for_all_households REAL,
+    all_private_dwellings INT,
+    average_number_of_people_per_household REAL,
+    median_weekly_household_income INT,
+    median_monthly_mortgage_repayments INT,
+    median_weekly_rent_b INT,
+    average_number_of_motor_vehicles_per_dwelling REAL
+)
+LANGUAGE plpgsql
+AS $$
+begin
+    return query
+    select id, suburb_name, state_name, post_code, people, male, female, median_age, families,
+    average_number_of_children_per_family, for_families_with_children, for_all_households,
+    all_private_dwellings, average_number_of_people_per_household, median_weekly_household_income,
+    median_monthly_mortgage_repayments, median_weekly_rent_b, average_number_of_motor_vehicles_per_dwelling
+    from summary_data
+    WHERE suburb_name ILIKE '%' || searchquery || '%'
+    AND summary_data.path ~ "root";
+end;$$;
+
+create function test_pull()
 returns table (
-  id uuid,
-  suburb_name text,
-  state_name text,
-  post_code numeric,
-  people INT,
-  male REAL,
-  female REAL,
-  median_age INT,
-  families INT,
-  average_number_of_children_per_family text,
-  for_families_with_children REAL,
-  for_all_households REAL,
-  all_private_dwellings INT,
-  average_number_of_people_per_household REAL,
-  median_weekly_household_income INT,
-  median_monthly_mortgage_repayments INT,
-  median_weekly_rent_b INT,
-  average_number_of_motor_vehicles_per_dwelling REAL
+id uuid,
+suburb_name text,
+state_name text,
+post_code numeric,
+people INT,
+male REAL,
+female REAL,
+median_age INT,
+families INT,
+average_number_of_children_per_family text,
+for_families_with_children REAL,
+for_all_households REAL,
+all_private_dwellings INT,
+average_number_of_people_per_household REAL,
+median_weekly_household_income INT,
+median_monthly_mortgage_repayments INT,
+median_weekly_rent_b INT,
+average_number_of_motor_vehicles_per_dwelling REAL
 )
 language plpgsql
 as $$
 begin
-    return query
-    select id, suburb_name, state_name, post_code, people, male, female, median_age, families,
-           average_number_of_children_per_family, for_families_with_children, for_all_households,
-           all_private_dwellings, average_number_of_people_per_household, median_weekly_household_income,
-           median_monthly_mortgage_repayments, median_weekly_rent_b, average_number_of_motor_vehicles_per_dwelling
-    from summary_data
-    where summary_data.path ~ 'root' and suburb_name ILIKE searchQuery;
+  return query
+  select id, suburb_name, state_name, post_code, people, male, female, median_age, families,
+          average_number_of_children_per_family, for_families_with_children, for_all_households,
+          all_private_dwellings, average_number_of_people_per_household, median_weekly_household_income,
+          median_monthly_mortgage_repayments, median_weekly_rent_b, average_number_of_motor_vehicles_per_dwelling
+  from summary_data
+  where summary_data.path ~ 'root';
 end;$$;
+
+
+CREATE FUNCTION get_first_10_rows()
+RETURNS SETOF summary_data
+LANGUAGE SQL
+AS $$
+  SELECT *
+  FROM summary_data
+  -- LIMIT 10;
+$$;
+
+CREATE FUNCTION test_pull_v2(searchquery TEXT)
+RETURNS TABLE (
+id uuid,
+suburb_name TEXT,
+state_name TEXT,
+post_code NUMERIC,
+people INT,
+male REAL,
+female REAL,
+median_age INT,
+families INT,
+average_number_of_children_per_family TEXT,
+for_families_with_children REAL,
+for_all_households REAL,
+all_private_dwellings INT,
+average_number_of_people_per_household REAL,
+median_weekly_household_income INT,
+median_monthly_mortgage_repayments INT,
+median_weekly_rent_b INT,
+average_number_of_motor_vehicles_per_dwelling REAL
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+RETURN QUERY
+SELECT id, suburb_name, state_name, post_code, people, male, female, median_age, families,
+average_number_of_children_per_family, for_families_with_children, for_all_households,
+all_private_dwellings, average_number_of_people_per_household, median_weekly_household_income,
+median_monthly_mortgage_repayments, median_weekly_rent_b, average_number_of_motor_vehicles_per_dwelling
+FROM summary_data
+WHERE suburb_name ILIKE CONCAT('%', searchquery, '%');
+END;$$;
+
+
 
 
 -- Add appropriate policies
@@ -83,3 +162,5 @@ DROP PUBLICATION IF EXISTS supabase_realtime CASCADE;
 CREATE PUBLICATION supabase_realtime WITH ( publish = 'insert, update, delete' );
 
 COMMIT;
+
+SELECT pg_stat_clear_snapshot();
