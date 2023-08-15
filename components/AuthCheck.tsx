08346cb393e697523a88023b9f6da8895229dafc
Wilfred Chen <wilfred@gmail.com>
2023-08-15 16:31:50 +1000

    signout only showing when user is logged in; it is correctly not displaying when user is logged out

--- components/AuthCheck.tsx
+++ components/AuthCheck.tsx
@@ -7,26 +7,60 @@ export default function AuthCheck({ children }: { children: React.ReactNode }) {
     const [isEmailVerified, setIsEmailVerified] = useState<boolean | null>(null);
 
     useEffect(() => {
-        const checkEmailVerification = async () => {
-            const supabase = createClientComponentClient();
-            const {
-                data: { session },
-            } = await supabase.auth.getSession();
+        const supabase = createClientComponentClient();
 
-            console.log(session);
-            console.log(session?.user.user_metadata.email_verified);
-
-            if (session?.user.user_metadata.email_verified === true) {
+        // Set up an auth state change listener
+        supabase.auth.onAuthStateChange((event, session) => {
+            if (event === "SIGNED_OUT") {
+                setIsEmailVerified(false);
+            } else if (event === "SIGNED_IN" && session?.user?.user_metadata?.email_verified) {
                 setIsEmailVerified(true);
             } else {
                 setIsEmailVerified(false);
             }
-        };
+        });
 
+        // Initial check
         checkEmailVerification();
     }, []);
 
+    async function checkEmailVerification() {
+        const supabase = createClientComponentClient();
+        const {
+            data: { session },
+        } = await supabase.auth.getSession();
+        if (session?.user?.user_metadata.email_verified) {
+            setIsEmailVerified(true);
+        } else {
+            setIsEmailVerified(false);
+        }
+    }
+
     console.log(`Email Verification Status: ${isEmailVerified}`);
+
+    // Initial Check
+
+    // useEffect(() => {
+    //     const checkEmailVerification = async () => {
+    //         const supabase = createClientComponentClient();
+    //         const {
+    //             data: { session },
+    //         } = await supabase.auth.getSession();
+
+    //         console.log(session);
+    //         console.log(session?.user.user_metadata.email_verified);
+
+    //         if (session?.user.user_metadata.email_verified === true) {
+    //             setIsEmailVerified(true);
+    //         } else if (!session) {
+    //             console.log("no email");
+    //             setIsEmailVerified(false);
+    //         }
+    //     };
+    //     checkEmailVerification();
+    // }, [isEmailVerified]);
+
+    // console.log(`Email Verification Status: ${isEmailVerified}`);
 
     if (isEmailVerified === null) {
         // Loading state or some other indicator
