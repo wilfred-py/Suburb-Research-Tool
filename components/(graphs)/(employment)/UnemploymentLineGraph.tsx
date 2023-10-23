232909582eefa143c49b7fd1af95494ded378217
Wilfred Chen <wilfred@gmail.com>
2023-10-23 22:59:11 +1100

    unemployment minMax working for y-axis

--- components/(graphs)/(employment)/UnemploymentLineGraph.tsx
+++ components/(graphs)/(employment)/UnemploymentLineGraph.tsx
@@ -36,6 +36,10 @@ export default function UnemploymentLineGraph(props: UnemploymentProps) {
     const [insufficientStateData, setInsufficientStateData] = useState(false);
     const [insufficientAustraliaData, setInsufficientAustraliaData] = useState(false);
 
+    // State to manage minimum and maximum data points for y-axis
+    const [dataMin, setDataMin] = useState(0);
+    const [dataMax, setDataMax] = useState(0);
+
     const supabase = createClientComponentClient();
 
     function deconstructSuburb(selectedSuburb: string | null) {
@@ -148,11 +152,13 @@ export default function UnemploymentLineGraph(props: UnemploymentProps) {
 
                     // * 2001
                     if (year == "2001") {
-                        const percentageSuburbUnemployment =
-                            data[0]["employment_data"]["Employment People aged 15 years and over"]["Unemployed"]["% of suburb"];
+                        const percentageSuburbUnemployment = parseFloat(
+                            data[0]["employment_data"]["Employment People aged 15 years and over"]["Unemployed"]["% of suburb"]
+                        );
 
-                        const percentageStateUnemployment =
-                            data[0]["employment_data"]["Employment People aged 15 years and over"]["Unemployed"]["% of state"];
+                        const percentageStateUnemployment = parseFloat(
+                            data[0]["employment_data"]["Employment People aged 15 years and over"]["Unemployed"]["% of state"]
+                        );
 
                         // Set suburbUnemployment if it exists
                         if (percentageSuburbUnemployment) {
@@ -179,10 +185,12 @@ export default function UnemploymentLineGraph(props: UnemploymentProps) {
                     }
                     // * 2006
                     else if (year == "2006") {
-                        const percentageSuburbUnemployment =
-                            data[0]["employment_data"]["Employment People aged 15 years and over"]["Unemployed"]["% of suburb"];
-                        const percentageStateUnemployment =
-                            data[0]["employment_data"]["Employment People aged 15 years and over"]["Unemployed"]["% of state"];
+                        const percentageSuburbUnemployment = parseFloat(
+                            data[0]["employment_data"]["Employment People aged 15 years and over"]["Unemployed"]["% of suburb"]
+                        );
+                        const percentageStateUnemployment = parseFloat(
+                            data[0]["employment_data"]["Employment People aged 15 years and over"]["Unemployed"]["% of state"]
+                        );
 
                         // Set suburbUnemployment if it exists
                         if (percentageSuburbUnemployment) {
@@ -207,14 +215,16 @@ export default function UnemploymentLineGraph(props: UnemploymentProps) {
 
                     // * 2011
                     else if (year == "2011") {
-                        const percentageSuburbUnemployment =
+                        const percentageSuburbUnemployment = parseFloat(
                             data[0]["employment_data"]["Employment People who reported being in the labour force, aged 15 years and over"][
                                 "Unemployed"
-                            ]["% of suburb"];
-                        const percentageStateUnemployment =
+                            ]["% of suburb"]
+                        );
+                        const percentageStateUnemployment = parseFloat(
                             data[0]["employment_data"]["Employment People who reported being in the labour force, aged 15 years and over"][
                                 "Unemployed"
-                            ]["% of state"];
+                            ]["% of state"]
+                        );
 
                         // Set suburbUnemployment if it exists
                         if (percentageSuburbUnemployment) {
@@ -239,14 +249,16 @@ export default function UnemploymentLineGraph(props: UnemploymentProps) {
 
                     // * 2016
                     else if (year == "2016") {
-                        const percentageSuburbUnemployment =
+                        const percentageSuburbUnemployment = parseFloat(
                             data[0]["employment_data"]["Employment People who reported being in the labour force, aged 15 years and over"][
                                 "Unemployed"
-                            ]["% of suburb"];
-                        const percentageStateUnemployment =
+                            ]["% of suburb"]
+                        );
+                        const percentageStateUnemployment = parseFloat(
                             data[0]["employment_data"]["Employment People who reported being in the labour force, aged 15 years and over"][
                                 "Unemployed"
-                            ]["% of state"];
+                            ]["% of state"]
+                        );
 
                         // Set suburbUnemployment if it exists
                         if (percentageSuburbUnemployment) {
@@ -271,8 +283,12 @@ export default function UnemploymentLineGraph(props: UnemploymentProps) {
 
                     // * 2021
                     else if (year == "2021") {
-                        const percentageSuburbUnemployment = data[0]["employment_data"]["Employment status"]["Unemployed"]["% of suburb"];
-                        const percentageStateUnemployment = data[0]["employment_data"]["Employment status"]["Unemployed"]["% of state"];
+                        const percentageSuburbUnemployment = parseFloat(
+                            data[0]["employment_data"]["Employment status"]["Unemployed"]["% of suburb"]
+                        );
+                        const percentageStateUnemployment = parseFloat(
+                            data[0]["employment_data"]["Employment status"]["Unemployed"]["% of state"]
+                        );
 
                         // Set suburbUnemployment if it exists
                         if (percentageSuburbUnemployment) {
@@ -329,7 +345,47 @@ export default function UnemploymentLineGraph(props: UnemploymentProps) {
         }
     }, [props.selectedSuburb]);
 
-    // console.log(`suburbUnemployment: ${suburbUnemployment}`);
+    // * useEffect hook to determine min and max for y-axis
+    useEffect(() => {
+        async function minMax(
+            suburbUnemployment: (number | null)[],
+            stateUnemployment: (number | null)[],
+            australiaUnemployment: (number | null)[]
+        ) {
+            // Define minimum and maximum variables
+            let dataMin: number = 100;
+            let dataMax: number = 0;
+
+            // Combine all 3 arrays
+            const combinedList = suburbUnemployment.concat(stateUnemployment, australiaUnemployment);
+
+            // Remove null values from combinedList
+            const cleanedList = combinedList.filter((value) => value !== null) as number[];
+            console.log(cleanedList);
+
+            // Iterate over items in arrays and determine lowest number for dataMin and highest number for dataMax
+            for (let i = 0; i < cleanedList.length; i++) {
+                if (cleanedList[i] !== null && cleanedList[i] < dataMin) {
+                    dataMin = cleanedList[i];
+                }
+                if (cleanedList[i] !== null && cleanedList[i] > dataMax) {
+                    dataMax = cleanedList[i];
+                }
+            }
+
+            setDataMin(dataMin);
+            setDataMax(dataMax);
+
+            console.log(dataMin);
+            console.log(dataMax);
+        }
+
+        minMax(suburbUnemployment, stateUnemployment, australiaUnemployment);
+        console.log(`dataMin: ${dataMin}`);
+        console.log(`dataMax: ${dataMax}`);
+    }, [suburbUnemployment, stateUnemployment, australiaUnemployment]);
+
+    console.log(`suburbUnemployment: ${suburbUnemployment}`);
     // console.log(`stateUnemployment: ${stateUnemployment}`);
     // console.log(`australiaUnemployment: ${australiaUnemployment}`);
 
@@ -352,7 +408,7 @@ export default function UnemploymentLineGraph(props: UnemploymentProps) {
             <XAxis dataKey="name">
                 <Label value="year" position="bottom" />
             </XAxis>
-            <YAxis tickCount={6} domain={["auto", "auto"]}>
+            <YAxis tickCount={6} domain={[0, dataMax + 2]}>
                 <Label value="%" position="insideLeft" />
             </YAxis>
             <Tooltip offset={50} cursor={false} />
