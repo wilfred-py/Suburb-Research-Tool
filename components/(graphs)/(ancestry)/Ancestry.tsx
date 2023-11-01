18681318d5c04cfc9af89084730e822aea0f1e1e
Wilfred Chen <wilfred@gmail.com>
2023-11-01 12:06:05 +1100

    added radar charts for all 2011, 2016, 2021; need to set up useState hook to render specific radar depending on year

--- components/(graphs)/(ancestry)/Ancestry.tsx
+++ components/(graphs)/(ancestry)/Ancestry.tsx
@@ -10,8 +10,7 @@ export default function Ancestry(props: AncestryProps) {
             <div className="flex flex-col border-2 border-blue-400 my-4">
                 <h1 className="text-2xl font-bold mx-auto">Ancestry</h1>
                 <div className="flex flex-row flex-wrap">
-                    <h1>2011</h1>
-                    <AncestryChart selectedSuburb={props.selectedSuburb} />
+                    <AncestryChart selectedSuburb={props.selectedSuburb} year={2011} />
                 </div>
             </div>
         </>
