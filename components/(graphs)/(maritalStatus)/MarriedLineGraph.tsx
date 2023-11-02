1ef1e0f40eb112808acac5935fbf610dfa5923b2
Wilfred Chen <wilfred@gmail.com>
2023-11-02 18:39:29 +1100

    marriedLineGraph working with minMax updated and 2001, 2006 x values removed

---
+++ components/(graphs)/(maritalStatus)/MarriedLineGraph.tsx
@@ -0,0 +1,390 @@
+"use client";
+
+import * as React from "react";
+import { useEffect, useState } from "react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer, Label, CartesianAxis } from "recharts";
+
+interface MarriedProps {
+    selectedSuburb: string | null;
+}
+
+interface IncomeDataItem {
+    income_data: any;
+}
+
+export default function MarriedLineGraph(props: MarriedProps) {
+    const [selectedSuburb, setSelectedSuburb] = useState<string | null>("");
+    const [selectedState, setSelectedState] = useState<string | null>("");
+
+    const [deconstructedSuburb, setDeconstructedSuburb] = useState<string | null>(null);
+    const [deconstructedState, setDeconstructedState] = useState<string | null>(null);
+
+    const [suburbMarried, setSuburbMarried] = useState<(number | null)[]>([null, null, null, null, null]);
+    const [stateMarried, setStateMarried] = useState<(number | null)[]>([null, null, null, null, null]);
+    const [australiaMarried, setAustraliaMarried] = useState<(number | null)[]>([null, null, null, null, null]);
+
+    // States to manage instances where data does not exist
+    const [insufficientSuburbData, setInsufficientSuburbData] = useState(false);
+
+    // State to manage minimum and maximum data points for y-axis
+    const [dataMin, setDataMin] = useState(0);
+    const [dataMax, setDataMax] = useState(0);
+
+    const supabase = createClientComponentClient();
+
+    function deconstructSuburb(selectedSuburb: string | null) {
+        // State Regex
+        const stateRegex = /^(.*?),\s*(VIC|NSW|ACT|WA|SA|TAS|NT|QLD|Other Territories)/;
+
+        // ! Suburb Name
+        // Create substrings based on stateRegex
+        const suburbMatch = selectedSuburb?.match(stateRegex);
+
+        // If it exists, return first match in suburbName
+        const suburbName = suburbMatch ? suburbMatch[1] : null;
+
+        // ! State Name
+        const stateName = suburbMatch ? suburbMatch[2] : null;
+
+        // ! Post Code
+        const postcode = selectedSuburb?.slice(-4);
+
+        // console.log(`suburbName: ${suburbName}`);
+        // console.log(`stateName: ${stateName}`);
+
+        setDeconstructedSuburb(suburbName);
+        setDeconstructedState(stateName);
+
+        return {
+            suburbName,
+            stateName,
+            postcode,
+        };
+    }
+
+    // * Function that fetches data for a specific year
+    async function fetchMarriedDataByYear(year: string, tableName: string, selectedSuburb: string | null) {
+        try {
+            const { suburbName, stateName } = deconstructSuburb(selectedSuburb);
+            setSelectedSuburb(suburbName);
+            setSelectedState(stateName);
+
+            const { data, error } = await supabase
+                .from(tableName)
+                .select("*")
+                .like("suburb_name", `%${suburbName}%`)
+                .eq("state_name", stateName);
+
+            if (error) {
+                console.error("Error fetching data:", error);
+                return null;
+            }
+
+            return { year, data };
+        } catch (error) {
+            console.error("Data fetch unsuccessful", error);
+            return null;
+        }
+    }
+
+    // * Function that checks if there is insufficient data in suburb to populate on line graphs
+    async function checkIfInsufficientData(arr: (number | null)[]) {
+        let numberCount = 0;
+        let nullCount = 0;
+
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] === null) {
+                nullCount++;
+            } else if (typeof arr[i] === "number") {
+                numberCount++;
+            }
+        }
+
+        // If only one year of data available or no data available at all
+        if ((numberCount === 1 && nullCount === 4) || nullCount === 5) {
+            console.log("insufficient data");
+            setInsufficientSuburbData(true);
+        }
+
+        // console.log(`HouseholdIncome nullCount: ${nullCount} \n HouseholdIncome numberCount: ${numberCount}`);
+
+        return (numberCount === 1 && nullCount === 4) || nullCount === 5;
+    }
+
+    useEffect(() => {
+        async function fetchData() {
+            // Clear suburbHouseholdIncome from previous search
+            setSuburbMarried([null, null, null, null, null]);
+            setStateMarried([null, null, null, null, null]);
+            setAustraliaMarried([null, null, null, null, null]);
+
+            // console.log(`selectedSuburb: ${props.selectedSuburb}`);
+            const years = ["2011", "2016", "2021"];
+            const dataPromises = years.map((year) => fetchMarriedDataByYear(year, `data_${year}`, props.selectedSuburb));
+
+            // Reset insufficient useState to false
+            setInsufficientSuburbData(false);
+
+            const newSuburbMarried = [...suburbMarried];
+            const newStateMarried = [...stateMarried];
+            const newAustraliaMarried = [...australiaMarried];
+
+            //  Wait for data to be fetched
+            const dataResults = await Promise.all(dataPromises);
+
+            // Filter out any null results (errors)
+            const validData = dataResults.filter((result): result is { year: string; data: any[] } => result !== null);
+
+            // Process the valid data and update state accordingly
+            validData.forEach((result) => {
+                const { year, data } = result;
+
+                try {
+                    // * 2001 - registered marriage recorded slightly different
+
+                    // * 2006 - registered marriage recorded slightly different
+
+                    // * 2011
+                    if (year == "2011") {
+                        const suburbMarried = data[0]["percentage_married_in_suburb"];
+                        const stateMarried = data[0]["percentage_married_in_state"];
+                        const australiaMarried = data[0]["percentage_married_in_australia"];
+
+                        // >> Suburb
+                        if (suburbMarried) {
+                            newSuburbMarried[2] = suburbMarried;
+                            setSuburbMarried(newSuburbMarried);
+                        } else if (!suburbMarried) {
+                            // ! If data is not available, remove element from array so line graph does not display it
+                            newSuburbMarried[2] = null;
+                            setSuburbMarried(newSuburbMarried);
+                        }
+
+                        // >> State
+                        if (stateMarried) {
+                            newStateMarried[2] = stateMarried;
+                            setStateMarried(newStateMarried);
+                        } else if (!stateMarried) {
+                            // ! If data is not available, remove element from array so line graph does not display it
+                            newStateMarried[2] = null;
+                            setStateMarried(newStateMarried);
+                        }
+
+                        // >> Australia
+                        if (australiaMarried) {
+                            newAustraliaMarried[2] = australiaMarried;
+                            setAustraliaMarried(newAustraliaMarried);
+                        } else if (!australiaMarried) {
+                            // ! If data is not available, remove element from array so line graph does not display it
+                            newAustraliaMarried[2] = null;
+                            setAustraliaMarried(newAustraliaMarried);
+                        }
+                    }
+
+                    // * 2016
+                    else if (year == "2016") {
+                        const suburbMarried = data[0]["percentage_married_in_suburb"];
+                        const stateMarried = data[0]["percentage_married_in_state"];
+                        const australiaMarried = data[0]["percentage_married_in_australia"];
+
+                        // >> Suburb
+                        if (suburbMarried) {
+                            newSuburbMarried[3] = suburbMarried;
+                            setSuburbMarried(newSuburbMarried);
+                        } else if (!suburbMarried) {
+                            // ! If data is not available, remove element from array so line graph does not display it
+                            newSuburbMarried[3] = null;
+                            setSuburbMarried(newSuburbMarried);
+                        }
+
+                        // >> State
+                        if (stateMarried) {
+                            newStateMarried[3] = stateMarried;
+                            setStateMarried(newStateMarried);
+                        } else if (!stateMarried) {
+                            // ! If data is not available, remove element from array so line graph does not display it
+                            newStateMarried[3] = null;
+                            setStateMarried(newStateMarried);
+                        }
+
+                        // >> Australia
+                        if (australiaMarried) {
+                            newAustraliaMarried[3] = australiaMarried;
+                            setAustraliaMarried(newAustraliaMarried);
+                        } else if (!australiaMarried) {
+                            // ! If data is not available, remove element from array so line graph does not display it
+                            newAustraliaMarried[3] = null;
+                            setAustraliaMarried(newAustraliaMarried);
+                        }
+                    }
+
+                    // * 2021
+                    else if (year === "2021") {
+                        const suburbMarried = data[0]["percentage_married_in_suburb"];
+                        const stateMarried = data[0]["percentage_married_in_state"];
+                        const australiaMarried = data[0]["percentage_married_in_australia"];
+
+                        // >> Suburb
+                        if (suburbMarried) {
+                            newSuburbMarried[4] = suburbMarried;
+                            setSuburbMarried(newSuburbMarried);
+                        } else if (!suburbMarried) {
+                            // ! If data is not available, remove element from array so line graph does not display it
+                            newSuburbMarried[4] = null;
+                            setSuburbMarried(newSuburbMarried);
+                        }
+
+                        // >> State
+                        if (stateMarried) {
+                            newStateMarried[4] = stateMarried;
+                            setStateMarried(newStateMarried);
+                        } else if (!stateMarried) {
+                            // ! If data is not available, remove element from array so line graph does not display it
+                            newStateMarried[4] = null;
+                            setStateMarried(newStateMarried);
+                        }
+
+                        // >> Australia
+                        if (australiaMarried) {
+                            newAustraliaMarried[4] = australiaMarried;
+                            setAustraliaMarried(newAustraliaMarried);
+                        } else if (!australiaMarried) {
+                            // ! If data is not available, remove element from array so line graph does not display it
+                            newAustraliaMarried[4] = null;
+                            setAustraliaMarried(newAustraliaMarried);
+                        }
+                    }
+                } catch (error) {
+                    console.error(`Error processing data for ${year}`, error);
+                    if (year === "2001") {
+                        console.log(`${year} data not available for ${selectedSuburb}`);
+                        newSuburbMarried[0] = null;
+                        setSuburbMarried(newSuburbMarried);
+                    } else if (year === "2006") {
+                        console.log(`2006 data not available for ${selectedSuburb}`);
+                        newSuburbMarried[1] = null;
+                        setSuburbMarried(newSuburbMarried);
+                    } else if (year === "2011") {
+                        console.log(`2011 data not available for ${selectedSuburb}`);
+                        newSuburbMarried[2] = null;
+                        setSuburbMarried(newSuburbMarried);
+                    } else if (year === "2016") {
+                        console.log(`2016 data not available for ${selectedSuburb}`);
+                        newSuburbMarried[3] = null;
+                        setSuburbMarried(newSuburbMarried);
+                    } else if (year === "2021") {
+                        console.log(`2021 data not available for ${selectedSuburb}`);
+                        newSuburbMarried[4] = null;
+                        setSuburbMarried(newSuburbMarried);
+
+                        // Check if there is insufficient data
+                        checkIfInsufficientData(suburbMarried);
+                    }
+                }
+            });
+        }
+
+        if (props.selectedSuburb) {
+            fetchData();
+        }
+    }, [props.selectedSuburb]);
+
+    // * useEffect hook to determine min and max for y-axis
+    useEffect(() => {
+        async function minMax(
+            suburbUnemployment: (number | null)[],
+            stateUnemployment: (number | null)[],
+            australiaUnemployment: (number | null)[]
+        ) {
+            // Define minimum and maximum variables
+            let dataMin: number = 9999;
+            let dataMax: number = 0;
+
+            // Combine all 3 arrays
+            const combinedList = suburbUnemployment.concat(stateUnemployment, australiaUnemployment);
+
+            // Remove null values from combinedList
+            const cleanedList = combinedList.filter((value) => value !== null) as number[];
+            // console.log(cleanedList);
+
+            // Iterate over items in arrays and determine lowest number for dataMin and highest number for dataMax
+            for (let i = 0; i < cleanedList.length; i++) {
+                if (cleanedList[i] !== null && cleanedList[i] < dataMin) {
+                    dataMin = cleanedList[i];
+                }
+                if (cleanedList[i] !== null && cleanedList[i] > dataMax) {
+                    dataMax = cleanedList[i];
+                }
+            }
+
+            setDataMin(dataMin);
+            setDataMax(dataMax);
+        }
+
+        minMax(suburbMarried, stateMarried, australiaMarried);
+    }, [suburbMarried, stateMarried, australiaMarried]);
+
+    // * <Recharts />
+
+    const data = [
+        { name: "2011", Suburb: suburbMarried[2], State: stateMarried[2], Australia: australiaMarried[2] },
+        { name: "2016", Suburb: suburbMarried[3], State: stateMarried[3], Australia: australiaMarried[3] },
+        { name: "2021", Suburb: suburbMarried[4], State: stateMarried[4], Australia: australiaMarried[4] },
+    ];
+
+    const renderLineChart = (
+        <LineChart width={600} height={400} data={data} margin={{ top: 30, right: 30, bottom: 30, left: 30 }}>
+            <Line type="natural" dataKey="Suburb" stroke="#219C90" strokeWidth={2.4} />
+            <Line type="natural" dataKey="State" stroke="#068FFF" strokeWidth={1.4} />
+            <Line type="natural" dataKey="Australia" stroke="#A90076" strokeWidth={1.4} />
+            <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+            <XAxis dataKey="name">
+                <Label value="year" position="bottom" />
+            </XAxis>
+            <YAxis tickCount={6} domain={[dataMin - 2, dataMax + 2]} padding={{ bottom: 30 }}>
+                <Label value="%" position="insideLeft" />
+            </YAxis>
+            <Tooltip offset={50} cursor={false} />
+            <Legend verticalAlign="top" height={36} align="center" />
+            <CartesianGrid y={40}></CartesianGrid>
+        </LineChart>
+    );
+
+    const insufficientDataLineChart = (
+        <LineChart width={600} height={400} data={data} margin={{ right: 30, bottom: 30, left: 30 }}>
+            <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+            <XAxis dataKey="name">
+                <Label value="year" position="bottom" />
+            </XAxis>
+            <YAxis tickCount={10} domain={[35, 75]}>
+                <Label value="$" position="insideLeft" />
+            </YAxis>
+            <Tooltip offset={50} cursor={false} />
+            <Legend verticalAlign="top" height={36} align="center" />
+            <CartesianGrid y={40}></CartesianGrid>
+        </LineChart>
+    );
+
+    return (
+        <div>
+            <div className="flex flex-col justify-center">
+                <h1 className="mt-4 mb-4 text-lg text-center font-bold">Married</h1>
+                <div className="mx-auto -mt-4">
+                    {insufficientSuburbData ? (
+                        <div className="flex flex-col justify-center">
+                            <span className="mt-2 text-center italic">
+                                Insufficient data in suburb to populate full-time employment trends.
+                            </span>
+                            {insufficientDataLineChart}
+                        </div>
+                    ) : (
+                        // * <Recharts />
+                        <div>{renderLineChart}</div>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ components/MaritalStatus.tsx
@@ -0,0 +1,18 @@
+// interface MaritalProps {
+//     selectedSuburb: string | null;
+// }
+
+// export default function Marital(props: MaritalProps) {
+//     return (
+//         <div className="flex flex-col border-2 border-blue-400 my-4">
+//             <h1 className="text-2xl font-bold mx-auto">Marital</h1>
+//             <div className="flex flex-row flex-wrap">
+//                 <MarriedLineGraph selectedSuburb={props.selectedSuburb} />
+//                 <SeparatedLineGraph selectedSuburb={props.selectedSuburb} />
+//                 <Divorced LineGraph selectedSuburb={props.selectedSuburb} />
+//                 <WidowedLineGraph selectedSuburb={props.selectedSuburb} />
+//                 <NeverMarriedLineGraph selectedSuburb={props.selectedSuburb} />
+//             </div>
+//         </div>
+//     );
+// }
