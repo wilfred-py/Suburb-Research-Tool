7e69b7cf332119a242df45c0cb37e4eef4a0e7fd
Wilfred Chen <wilfred@gmail.com>
2023-11-28 17:20:03 +1100

    navbar responsive in both auth and unauth states

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -2,15 +2,52 @@
 
 import Link from "next/link";
 import AuthCheck from "@/components/AuthCheck";
-import { SignInButton, SignOutButton } from "@/components/AuthButtons";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { SignInButton, HamburgerSignOutButton, NavSignOutButton } from "@/components/AuthButtons";
 import { Twirl as Hamburger } from "hamburger-react";
 import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
 
 export default function NavBar() {
+    // Handle hamburger open state
     const [isOpen, setOpen] = useState(false);
 
+    // Handle auth state
+    const [isEmailVerified, setIsEmailVerified] = useState<boolean | null>(false);
+
+    // * Check auth
+    useEffect(() => {
+        const supabase = createClientComponentClient();
+
+        // Set up an auth state change listener
+        supabase.auth.onAuthStateChange((event, session) => {
+            if (event === "SIGNED_OUT") {
+                setIsEmailVerified(false);
+            } else if (event === "SIGNED_IN" && session?.user?.user_metadata?.email_verified) {
+                setIsEmailVerified(true);
+            } else {
+                setIsEmailVerified(false);
+            }
+        });
+
+        // Initial check
+        checkEmailVerification();
+    }, []);
+
+    async function checkEmailVerification() {
+        const supabase = createClientComponentClient();
+        const {
+            data: { session },
+        } = await supabase.auth.getSession();
+        if (session?.user?.user_metadata.email_verified) {
+            setIsEmailVerified(true);
+        } else {
+            setIsEmailVerified(false);
+        }
+    }
+
+    // * Close hamburger menu if width > 1024px
     useEffect(() => {
         let resizeTimer: any;
 
@@ -51,28 +88,30 @@ export default function NavBar() {
 
                         <div className="my-auto  mr-2 text-offWhite">
                             <ul className="flex flex-row place-items-center font-semibold list-none space-x-4">
-                                <li className="hidden lg:block">
-                                    <AuthCheck>
+                                {isEmailVerified && (
+                                    <li className="hidden lg:block mr-4">
                                         <Link href={"/dashboard"}>Dashboard</Link>
-                                    </AuthCheck>
-                                </li>
+                                    </li>
+                                )}
 
                                 <li className="hidden lg:block">
                                     <SignInButton />
                                 </li>
 
-                                <li className="hidden lg:block">
-                                    <Button className="rounded-sm py-2 px-5 bg-white text-navigationBarBlue hover:bg-hoverBlue hover:shadow-sm hover:text-black">
-                                        Get Started
-                                        <Link href={"/dashboard/sign-up"}></Link>
-                                    </Button>
-                                </li>
+                                {!isEmailVerified && (
+                                    <li className="hidden lg:block">
+                                        <Button className="rounded-sm py-2 px-5 bg-white text-navigationBarBlue hover:bg-hoverBlue hover:shadow-sm hover:text-black">
+                                            Get Started
+                                            <Link href={"/dashboard/sign-up"}></Link>
+                                        </Button>
+                                    </li>
+                                )}
 
-                                <li className="hidden lg:block">
-                                    <AuthCheck>
-                                        <SignOutButton />
-                                    </AuthCheck>
-                                </li>
+                                {isEmailVerified && (
+                                    <li className="hidden lg:block">
+                                        <NavSignOutButton />
+                                    </li>
+                                )}
                             </ul>
                             <div className={`lg:hidden`}>
                                 <Hamburger
@@ -95,14 +134,14 @@ export default function NavBar() {
                 <nav id="mobile-nav" className="lg:hidden fixed inset-x-0 bottom-0 top-16 z-50 overflow-x-hidden bg-white">
                     <div className="flex flex-col fixed bottom-0 left-0 top-16 w-screen overflow-y-hidden  ">
                         <ul className="flex flex-col w-full leading-tight ">
-                            <li className="hover:underline hover:underline-offset-1">
-                                <AuthCheck>
+                            {isEmailVerified && (
+                                <li className="hover:underline hover:underline-offset-1">
                                     <button className="flex items-center justify-between h-16 w-full py-3 pl-7 pr-10 sm:pl-12 sm:pr-14 border-b hover:bg-hoverBlue">
                                         <Link href="/dashboard/">Dashboard</Link>
                                         <ArrowRight strokeWidth={1} />
                                     </button>
-                                </AuthCheck>
-                            </li>
+                                </li>
+                            )}
                             <li className="m-0 bg-none p-0 inset-x-0 w-full">
                                 <button className="flex items-center justify-between h-16 w-full py-3 pl-7 pr-10 sm:pl-12 sm:pr-14 border-b hover:bg-hoverBlue">
                                     About
@@ -116,14 +155,21 @@ export default function NavBar() {
                                 </button>
                             </li>
                         </ul>
-                        <div className="mb-20 mt-auto flex flex-col items-center gap-3 p-5 md:flex-row md:gap-6 md:p-6 md:px-9">
-                            <Button className="h-10 w-full px-4 py-3 bg-gray text-black">
-                                <Link href="/dashboard/sign-in">Sign in</Link>
-                            </Button>
-                            <Button className="h-10 w-full px-4 py-3 bg-navigationBarBlue">
-                                <Link href="/dashboard/sign-up"> Get started</Link>
-                            </Button>
-                        </div>
+                        {!isEmailVerified && (
+                            <div className="mb-20 mt-auto flex flex-col items-center gap-3 p-5 md:flex-row md:gap-6 md:p-6 md:px-9">
+                                <Button className="h-10 w-full px-4 py-3 bg-gray text-black">
+                                    <Link href="/dashboard/sign-in">Sign in</Link>
+                                </Button>
+                                <Button className="h-10 w-full px-4 py-3 bg-navigationBarBlue">
+                                    <Link href="/dashboard/sign-up"> Get started</Link>
+                                </Button>
+                            </div>
+                        )}
+                        {isEmailVerified && (
+                            <div className="mb-20 mt-auto flex flex-col items-center gap-3 p-5 md:flex-row md:gap-6 md:p-6 md:px-9">
+                                <HamburgerSignOutButton />
+                            </div>
+                        )}
                     </div>
                 </nav>
             )}
