a14cedfbda338b49a504b68e4f1babde34eac534
Wilfred Chen <wilfred@gmail.com>
2023-11-29 22:38:09 +1100

    updated max height for #About container

--- components/About.tsx
+++ components/About.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function About() {
     return (
         <div className="pt-16">
-            <div className="flex flex-col place-items-center w-full h-[600px] mx-auto px-5 sm:px-9 md:px-10 lg:px-12">
+            <div className="flex flex-col place-items-center w-full max-h-96  mx-auto px-5 sm:px-9 md:px-10 lg:px-12">
                 <div className="w-full mx-auto max-w-8xl py-16 md:pt-28">
                     <div className="flex flex-col w-full gap-8">
                         <h1 className="w-full max-w-[70%] mr-auto font-inter600 tracking-tighter text-5xl leading-tight">
