e3ed41dd03b42f9c7151cfb8675e37b69a195656
Wilfred Chen <wilfred@gmail.com>
2023-12-07 16:06:10 +1100

    heroicons added to <h1> tags on landingPage

--- components/About.tsx
+++ components/About.tsx
@@ -2,8 +2,6 @@
 
 import ExampleStateDwellingStackedAreaChart from "./(graphs)/(dwelling)/(examples)/ExampleStateDwellingStackedAreaChart";
 import ExampleSuburbDwellingStackedAreaChart from "./(graphs)/(dwelling)/(examples)/ExampleSuburbDwellingStackedAreaChart";
-import ExampleDwellingStackedAreaChart from "./(graphs)/(dwelling)/(examples)/ExampleSuburbDwellingStackedAreaChart";
-import DwellingStackedAreaChart from "./(graphs)/(dwelling)/DwellingStackedAreaChart";
 import ExampleFTE from "./(graphs)/(employment)/(examples)/ExampleFTE";
 import ExamplePartTimeEmploymentLineGraph from "./(graphs)/(employment)/(examples)/ExamplePartTimeEmploymentLineGraph";
 import ExampleUnemploymentLineGraph from "./(graphs)/(employment)/(examples)/ExampleUnemploymentLineGraph";
@@ -25,7 +23,26 @@ export default function About() {
                     </div>
                     <hr></hr>
                     <div className="flex flex-col md:justify-center">
-                        <h1 className="mt-4 mb-1 font-inter700 text-2xl text-center lg:text-left">Employment</h1>
+                        <div className="flex flex-row">
+                            <h1 className="mt-4 mb-1 font-inter700 text-2xl text-center lg:text-left">Employment</h1>
+                            <div className="flex flex-col justify-center mt-2 ml-2">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth={1.5}
+                                    stroke="currentColor"
+                                    className="w-6 h-6"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z"
+                                    />
+                                </svg>
+                            </div>
+                        </div>
+
                         <h3 className="mb-4 font-inter400 text-center lg:text-left">
                             Compare suburb employment rates to a suburb's state and Australia
                         </h3>
@@ -45,7 +62,26 @@ export default function About() {
 
                     <hr className="mt-10"></hr>
                     <div className="flex flex-col md:justify-center">
-                        <h1 className="mt-4 mb-1 font-inter700 text-2xl text-center lg:text-left">Dwelling Structure</h1>
+                        <div className="flex flex-row">
+                            <h1 className="mt-4 mb-1 font-inter700 text-2xl text-center lg:text-left">Dwelling Structure</h1>
+                            <div className="flex flex-col justify-center mt-2 ml-2">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth={1.5}
+                                    stroke="currentColor"
+                                    className="w-6 h-6"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205l3 1m1.5.5l-1.5-.5M6.75 7.364V3h-3v18m3-13.636l10.5-3.819"
+                                    />
+                                </svg>
+                            </div>
+                        </div>
+
                         <h3 className="mb-4 font-inter400 text-center lg:text-left">
                             Discover the types of homes that exist in a suburb and its state over time
                         </h3>
@@ -61,7 +97,26 @@ export default function About() {
 
                     <hr className="mt-10"></hr>
                     <div className="flex flex-col md:justify-center">
-                        <h1 className="mt-4 mb-1 font-inter700 text-2xl text-center lg:text-left">Income</h1>
+                        <div className="flex flex-row">
+                            <h1 className="mt-4 mb-1 font-inter700 text-2xl text-center lg:text-left">Income</h1>
+                            <div className="flex flex-col justify-center mt-2 ml-2">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth={1.5}
+                                    stroke="currentColor"
+                                    className="w-6 h-6"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z"
+                                    />
+                                </svg>
+                            </div>
+                        </div>
+
                         <h3 className="mb-4 font-inter400 text-center lg:text-left">Analyse personal and household income trends</h3>
                     </div>
 
