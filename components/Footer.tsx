0a331fc9570f29c35375e387f3b3236b5f6a735f
Wilfred Chen <wilfred@gmail.com>
2023-12-09 17:21:04 +1100

    fixed margin top for employment in #highlights

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,33 +3,35 @@ import Link from "next/link";
 export default function Footer() {
     return (
         <div className="w-full h-96 pt-10 mx-auto px-5 sm:px-9 md:px-10 lg:px-12 xl:px-24 2xl:px-40 3xl:px-52 4xl:px-72 5xl:px-96 6xl:px-[440px] 7xl:px-[500px] 8xl:px-[600px] bg-footerBlue">
-            <div className="flex flex-col mt-2 ">
+            <div className="flex flex-col lg:flex-row">
                 <div className="flex flex-col ">
-                    <Link href="/" className="font-inter700 text-2xl">
-                        Suburb IQ
-                    </Link>
+                    <h1 className="font-inter700 text-2xl">Suburb IQ</h1>
                 </div>
 
-                <div className="flex flex-col mt-4 space-y-2">
+                <div className="flex flex-col sm:max-lg:mt-4 space-y-2">
                     <h1 className="font-inter600">Get started</h1>
-                    <Link href="/dashboard/sign-in">Sign in</Link>
-                    <Link href="/dashboard/sign-up">Sign up</Link>
+                    <Link href="/dashboard/sign-in" className="inline-block w-1/4">
+                        Sign in
+                    </Link>
+                    <Link href="/dashboard/sign-up" className="w-1/4">
+                        Sign up
+                    </Link>
                 </div>
 
                 <div className="flex flex-col mt-10 space-y-2">
                     <h1 className="font-inter600">Support</h1>
+                    <a href={"https://suburb-iq.canny.io/bug-reports-features-requests"} target="_blank">
+                        <span className="inline-block">Request a new feature</span>
+                    </a>
+                    <a href={"https://suburb-iq.canny.io/bug-reports-features-requests"} target="_blank">
+                        Report a bug
+                    </a>
                     <a
                         href="mailto:suburb.iq.feedback@gmail.com?subject=Feedback&body=Hi Suburb IQ team, "
                         target="_blank"
                         rel="noopener noreferrer"
                     >
                         Contact us
-                    </a>
-                    <a href={"https://suburb-iq.canny.io/bug-reports-features-requests"} target="_blank">
-                        Report a bug
-                    </a>
-                    <a href={"https://suburb-iq.canny.io/bug-reports-features-requests"} target="_blank">
-                        <span className="">Request a new feature</span>
                     </a>
                 </div>
             </div>
