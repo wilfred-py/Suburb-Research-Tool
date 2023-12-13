3d790e835c1f91d570678df8607f4d9b9c2cffe8
Wilfred Chen <wilfred@gmail.com>
2023-12-13 19:07:03 +1100

    suburb top-down photo added to auth screen

--- app/(dashboard)/dashboard/(auth)/sign-up/page.tsx
+++ app/(dashboard)/dashboard/(auth)/sign-up/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function SignUp() {
     return (
         <div className="">
-            <h1 className="text-3xl">Get Started</h1>
+            <h1 className="text-3xl font-inter500">Get Started</h1>
             <p className="mt-1 mb-10 text-sm font-normal">Create a new account</p>
             <AuthForm />
             <p className="text-center text-sm font-normal">
