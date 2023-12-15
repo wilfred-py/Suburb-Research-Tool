bdaac0c15572fa15c1eeb62df088b646d9bceec9
Wilfred Chen <wilfred@gmail.com>
2023-12-15 17:12:08 +1100

    changed right-panel hidden below lg viewport on auth-page

--- app/(dashboard)/dashboard/(auth)/layout.tsx
+++ app/(dashboard)/dashboard/(auth)/layout.tsx
@@ -5,7 +5,7 @@ import image from "@/public/Auth_Page_Images/Suburb Top-down.jpg";
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
     return (
         <section className="min-h-screen flex-1 flex flex-row overflow-hidden">
-            <div id="left-panel" className="flex flex-col relative xl:w-1/3 w-full ">
+            <div id="left-panel" className="flex flex-col relative lg:w-1/3 w-full ">
                 <div id="logo" className="xl:px-auto p-10">
                     <Link className="text-2xl font-bold" href={"/"}>
                         SuburbIQ
@@ -13,30 +13,29 @@ export default function AuthLayout({ children }: { children: React.ReactNode }) 
                 </div>
 
                 {/* Auth Form */}
-                <div className="flex-1 flex flex-col justify-center place-items-center px-7">
-                    <div className="w-96">{children}</div>
+                <div className="flex-1 flex flex-col justify-center place-items-center ">
+                    <div className="w-full px-8">{children}</div>
                 </div>
             </div>
 
-            <div
-                id="right-panel"
-                className="flex flex-col flex-1 relative xl:flex sm:max-xl:hidden max-sm:hidden justify-center items-center"
-            >
+            <div id="right-panel" className="flex flex-col flex-1 relative lg:w-2/3 w-full justify-center items-center max-lg:hidden">
                 {/* <h1 className="px-10 text-offWhite text-3xl font-inter600">Discover demographic trends in Australia's suburbs</h1>
                     <h2 className="w-full pt-6 px-10 text-offWhite">
                         Join the growing community of buyers and renters using Suburb IQ to help them make more informed decisions before
                         buying a home or investment.
                     </h2> */}
                 {/* <Image fill={true} src={"/../Auth_Page_Images/Adelaide.jpg"} alt="Adelaide Photo" className="object-cover" /> */}
-                <Image
-                    fill={true}
-                    src={image}
-                    alt="Top-down Suburb Photo"
-                    className="object-cover"
-                    priority={true}
-                    quality={100}
-                    placeholder="blur"
-                />
+                <div>
+                    <Image
+                        fill={true}
+                        src={image}
+                        alt="Top-down Suburb Photo"
+                        className="object-cover"
+                        priority={true}
+                        quality={100}
+                        placeholder="blur"
+                    />
+                </div>
             </div>
         </section>
     );
