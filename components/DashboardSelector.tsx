57c613a0e0792b858f686774d81a6059403ef9da
Wilfred Chen <wilfred@gmail.com>
2024-01-02 21:23:28 +1100

    added <span> to <button /> on dashboard selector component - responsiveness needs work on all views

--- components/DashboardSelector.tsx
+++ components/DashboardSelector.tsx
@@ -9,9 +9,9 @@ export default function DashboardSelector(props: DashboardSelectorProps) {
     // * Build className conditionally
     const getButtonCSS = (viewName: string) => {
         if (viewName === props.selectedView) {
-            return `bg-navigationBarBlue text-white font-medium p-0.5 my-2 border-1 border-navigationBarBlue rounded rounded-md hover:cursor-pointer select-none shadow-sm shadow-navigationBarBlue transition hover:delay-200`;
+            return `relative focus:outline-none focus:ring-0 whitespace-nowrap mr-2 md:mr-0 md:ml-2.5 px-4 py-1 md:py-2 hover:bg-hoverBlue text-left leading-6 text-black font-normal border-1 border-navigationBarBlue rounded rounded-sm hover:cursor-pointer select-none`;
         } else {
-            return `bg-white  rounded rounded-md p-0.5 my-2 hover:bg-navigationBarBlue hover:text-white hover:font-medium hover:cursor-pointer select-none transition hover:delay-200`;
+            return `relative focus:outline-none focus:ring-0 whitespace-nowrap mr-2 md:mr-0 md:ml-2.5 px-4 py-1 md:py-2 hover:bg-hoverBlue text-left hover:text-gray-900 leading-6 text-black font-normal border-1 border-navigationBarBlue rounded rounded-sm hover:cursor-pointer select-none`;
         }
     };
 
@@ -21,20 +21,39 @@ export default function DashboardSelector(props: DashboardSelectorProps) {
     };
 
     return (
-        <div className="flex flex-row justify-evenly items-center w-[400px] h-10 border-2 border-gray-300 rounded-md shadow-md bg-white">
-            <div className={getButtonCSS("Overview")}>
-                <button onClick={() => handleViewClick("Overview")}>Overview</button>
-            </div>
-            <div className={getButtonCSS("Housing Details")}>
-                <button onClick={() => handleViewClick("Housing Details")}>Housing Details</button>
-            </div>
-
-            <div className={getButtonCSS("Demographic")}>
-                <button onClick={() => handleViewClick("Demographic")}>Demographic</button>
-            </div>
-            <div className={getButtonCSS("Lifestyle")}>
-                <button onClick={() => handleViewClick("Lifestyle")}>Lifestyle</button>
-            </div>
+        <div className="flex flex-row md:flex-col min-w-[10rem] h-1/4 md:space-y-1 md:py-2.5 md:mx-0 mb-6 md:mb-0 md:top-[4.5rem] md:sticky justify-evenly">
+            <button className={getButtonCSS("Overview")} onClick={() => handleViewClick("Overview")}>
+                {props.selectedView == "Overview" ? (
+                    <span className="hidden md:block absolute w-0.5 h-full -left-2.5 top-0 bg-buttonBlue"></span>
+                ) : (
+                    ""
+                )}
+                Overview
+            </button>
+            <button className={getButtonCSS("Housing Details")} onClick={() => handleViewClick("Housing Details")}>
+                {props.selectedView == "Housing Details" ? (
+                    <span className="hidden md:block absolute w-0.5 h-full -left-2.5 top-0 bg-buttonBlue"></span>
+                ) : (
+                    ""
+                )}
+                Housing Details
+            </button>
+            <button className={getButtonCSS("Demographic")} onClick={() => handleViewClick("Demographic")}>
+                {props.selectedView == "Demographic" ? (
+                    <span className="hidden md:block absolute w-0.5 h-full -left-2.5 top-0 bg-buttonBlue"></span>
+                ) : (
+                    ""
+                )}
+                Demographic
+            </button>
+            <button className={getButtonCSS("Lifestyle")} onClick={() => handleViewClick("Lifestyle")}>
+                {props.selectedView == "Lifestyle" ? (
+                    <span className="hidden md:block absolute w-0.5 h-full -left-2.5 top-0 bg-buttonBlue"></span>
+                ) : (
+                    ""
+                )}
+                Lifestyle
+            </button>
         </div>
     );
 }
