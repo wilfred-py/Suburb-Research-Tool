46a6852ef5f434fce06dae4ad28f271065bbca70
Wilfred Chen <wilfred@gmail.com>
2024-01-02 22:43:46 +1100

    updated max width on parent container of views

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -56,7 +56,7 @@ export default function Dashboard() {
                             </div>
                         </div>
 
-                        <div className="max-w-[1280px] mx-auto mt-8">
+                        <div className="max-w-[2108px] mt-8">
                             <div className="w-full flex flex-col md:flex-row">
                                 {/* Dashboard selector */}
                                 <div className="">
