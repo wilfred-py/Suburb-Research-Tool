5f052211b96d4a1694a29df6f8eb8e5c1bf71eb1
Wilfred Chen <wilfred@gmail.com>
2024-01-02 21:40:38 +1100

    bg-hoverBlue added to selected view

--- components/DashboardSelector.tsx
+++ components/DashboardSelector.tsx
@@ -9,9 +9,9 @@ export default function DashboardSelector(props: DashboardSelectorProps) {
     // * Build className conditionally
     const getButtonCSS = (viewName: string) => {
         if (viewName === props.selectedView) {
-            return `relative focus:outline-none focus:ring-0 whitespace-nowrap mr-2 md:mr-0 md:ml-2.5 px-4 py-1 md:py-2 hover:bg-hoverBlue text-left leading-6 text-black font-normal border-1 border-navigationBarBlue rounded rounded-sm hover:cursor-pointer select-none`;
+            return `relative focus:outline-none focus:ring-0 whitespace-nowrap mr-2 md:mr-0 md:ml-2.5 p-2 py-1 md:py-2 bg-hoverBlue hover:bg-hoverBlue text-left leading-6 text-black font-normal border-1 border-navigationBarBlue rounded rounded-sm hover:cursor-pointer select-none`;
         } else {
-            return `relative focus:outline-none focus:ring-0 whitespace-nowrap mr-2 md:mr-0 md:ml-2.5 px-4 py-1 md:py-2 hover:bg-hoverBlue text-left hover:text-gray-900 leading-6 text-black font-normal border-1 border-navigationBarBlue rounded rounded-sm hover:cursor-pointer select-none`;
+            return `relative focus:outline-none focus:ring-0 whitespace-nowrap mr-2 md:mr-0 md:ml-2.5 p-2 py-1 md:py-2 hover:bg-hoverBlue text-left hover:text-gray-900 leading-6 text-black font-normal border-1 border-navigationBarBlue rounded rounded-sm hover:cursor-pointer select-none`;
         }
     };
 
@@ -21,7 +21,7 @@ export default function DashboardSelector(props: DashboardSelectorProps) {
     };
 
     return (
-        <div className="flex flex-row md:flex-col min-w-[10rem] h-1/4 md:space-y-1 md:py-2.5 md:mx-0 mb-6 md:mb-0 md:top-[4.5rem] md:sticky justify-evenly">
+        <div className="flex flex-row  md:flex-col min-w-[10rem] h-1/4 md:space-y-1 md:py-2.5 md:mx-0 mb-6 md:mb-0 md:top-[4.5rem] md:sticky justify-evenly">
             <button className={getButtonCSS("Overview")} onClick={() => handleViewClick("Overview")}>
                 {props.selectedView == "Overview" ? (
                     <span className="hidden md:block absolute w-0.5 h-full -left-2.5 top-0 bg-buttonBlue"></span>
