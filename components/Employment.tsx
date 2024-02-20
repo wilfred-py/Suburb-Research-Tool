2add57acb29a2d86d958bbf057bda05075f53dad
Wilfred Chen <wilfred@gmail.com>
2024-02-20 16:49:39 +1100

    Make Employment parent container responsive

--- components/Employment.tsx
+++ components/Employment.tsx
@@ -8,12 +8,22 @@ interface EmploymentProps {
 
 export default function Employment(props: EmploymentProps) {
     return (
-        <div className="flex flex-col border-2 border-blue-400 my-4">
-            <h1 className="text-2xl font-bold mx-auto">Employment</h1>
-            <div className="flex flex-row flex-wrap">
-                <FullTimeEmploymentLineGraph selectedSuburb={props.selectedSuburb} />
-                <PartTimeEmploymentLineGraph selectedSuburb={props.selectedSuburb} />
-                <UnemploymentLineGraph selectedSuburb={props.selectedSuburb} />
+        <div className="flex flex-col my-4 w-[99%]">
+            <h1 className="text-center text-2xl font-bold mx-auto">Employment</h1>
+            <div className="flex flex-col lg:flex-row flex-wrap min-h-full ">
+                <div className="flex flex-col lg:w-1/2 h-full">
+                    <div className="flex-1">
+                        <FullTimeEmploymentLineGraph selectedSuburb={props.selectedSuburb} />
+                    </div>
+                    <div className="flex-1">
+                        <PartTimeEmploymentLineGraph selectedSuburb={props.selectedSuburb} />
+                    </div>
+                </div>
+                <div className="flex flex-col lg:w-1/2 h-full">
+                    <div className="flex-1">
+                        <UnemploymentLineGraph selectedSuburb={props.selectedSuburb} />
+                    </div>
+                </div>
             </div>
         </div>
     );
