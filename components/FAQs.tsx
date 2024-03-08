610005c6ee666de9c70b807feb57d2eae51f04db
Wilfred Chen <wilfred@gmail.com>
2024-03-08 17:06:48 +1100

    Update features href in FAQs

--- components/FAQs.tsx
+++ components/FAQs.tsx
@@ -38,7 +38,7 @@ export default function FAQs() {
                         <AccordionContent>
                             We're always looking to implement new features that are useful to the community. If you have a great idea, let
                             us know
-                            <a data-canny-link href="https://suburbiq.canny.io" target="_blank">
+                            <a data-canny-link href="https://suburbiq.canny.io/feature-requests" target="_blank">
                                 {" "}
                                 <span className="text-left font-bold underline">here</span>
                             </a>{" "}
