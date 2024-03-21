e6df12ec2eda9f99fd8e1172de9b9af631b1aff7
Wilfred Chen <wilfred@gmail.com>
2024-03-21 12:20:27 +1100

    Hide Map component on mobile viewports

--- components/(dashboardViews)/OverviewView.tsx
+++ components/(dashboardViews)/OverviewView.tsx
@@ -12,7 +12,7 @@ export default function OverviewView(props: OverviewProps) {
         <>
             <div className="w-full h-full flex flex-row mobile-s:max-lg:flex-col">
                 <div className="lg:w-6/12 h-full flex flex-col mb-10 px-1 pb-1 md:max-lg:max-w-[99%]">
-                    <div className="w-full h-[520px] border border-gray-200 rounded-md shadow-lg hover:shadow-xl">
+                    <div className="mobile-s:max-mobile-l:hidden w-full h-[520px] border border-gray-200 rounded-md shadow-lg hover:shadow-xl">
                         <Map selectedSuburb={props.selectedSuburb} />
                     </div>
 
